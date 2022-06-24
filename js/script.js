11a3d746b76f3e2483daff7866c24b30c6a3bd57
marcocalligaris <marcocally@gmail.com>
2022-06-24 17:24:47 +0200

    email check

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Mail
+// Crea (inventa) una lista di inidirizzi mail autorizzati.
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa in console un messaggio appropriato sull’esito del controllo. ("Benvenuto" se la mail è nella lista, "Spiacenti" se non lo è);
+
+// * Creo l'array con gli indirizzi e-mail
+
+const addresses = ["giovanni@gmail.com", "maria@hotmail.it", "samuele@yahoo.com", "pietro@gmail.com", "alessandra@libero.it"];
+console.log(addresses);
+
+let email = prompt('Scrivi qui il tuo indirizzo email.');
+
+for(let i = 0; i < addresses.length; i++) {
+
+    if(email = addresses[i]) {
+        console.log('Benvenuto!');
+    } else {
+        console.log('Spiacenti');
+    }
+}
+
+
+
