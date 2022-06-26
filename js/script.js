e043baf2ffc9234cd431639280986e34d4e897a4
marcocalligaris <marcocally@gmail.com>
2022-06-26 12:48:03 +0200

    Fine esercizio email

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-// Mail
-// Crea (inventa) una lista di inidirizzi mail autorizzati.
-// Chiedi all’utente la sua email,
-// controlla che sia nella lista di chi può accedere,
-// stampa in console un messaggio appropriato sull’esito del controllo. ("Benvenuto" se la mail è nella lista, "Spiacenti" se non lo è);
-
-// * Creo l'array con gli indirizzi e-mail
-
 const addresses = ["giovanni@gmail.com", "maria@hotmail.it", "samuele@yahoo.com", "pietro@gmail.com", "alessandra@libero.it"];
 const userEmail = prompt('Scrivi qui il tuo indirizzo email.');
 
@@ -15,6 +7,6 @@ const userEmail = prompt('Scrivi qui il tuo indirizzo email.');
         console.log('Benvenuto!')
     } else {
         alert('Dati errati o utente non autorizzato.')
-        console.log('Spiacenti')
+        console.log('Dati errati o utente non autorizzato.')
     }
 
